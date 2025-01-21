09fcd848705bde61447c7863b710444c9abd8a9f
lemeshking <lemeshkinggg@gmail.com>
2025-01-21 14:41:51 +0700

    +structProject, +libFunctions

---
+++ src/lib/lib.h
@@ -0,0 +1,12 @@
+#include<string>
+
+#ifndef infotecs_TEST_H
+#define infotecs_TEST_H
+
+
+
+void sortAndChange(std::string& str);
+int calculateSum(const std::string& str);
+bool analyzeString(const std::string &str);
+
+#endif
